ac58119367c9c9295a4195cbd8d19a4243c59c11
kofkuiper <kofkuiper@gmail.com>
2022-05-07 14:25:14 +0700

    contract: Add test file.

---
+++ contract/test/airdrop.ts
@@ -0,0 +1,74 @@
+import { expect } from "chai";
+import { arrayify } from "ethers/lib/utils";
+import { ethers } from "hardhat";
+import { BigNumber, Signer } from "ethers";
+import * as dotenv from "dotenv";
+import { Airdrop, Airdrop__factory } from "../typechain";
+
+dotenv.config();
+
+function toWei(value: Number) {
+    return ethers.utils.parseEther(value.toString())
+}
+
+function fromWei(value: BigNumber) {
+    return Number(ethers.utils.formatEther(value))
+}
+
+describe("Airdrop test", function () {
+    let airdrop: Airdrop
+    let signer: Signer
+    let claimer: Signer
+
+    // Private key of signer
+    const privateKey = process.env.PRIVATE_KEY ? process.env.PRIVATE_KEY : ''
+
+    beforeEach(async function () {
+        [signer, claimer] = await ethers.getSigners()
+        airdrop = await new Airdrop__factory(signer).deploy(await signer.getAddress())
+    })
+
+    describe("Deployment", async function () {
+        it("Should deployed Airdrop", async function () {
+            expect(airdrop.address).not.to.eq(null)
+        })
+
+        it("Should set the right owner", async function () {
+            expect(await airdrop.owner()).to.eq(await signer.getAddress())
+        })
+
+    })
+
+    describe("Claim Airdrop", function () {
+
+        it("Should claim airdrop if signature has signed from signer", async function () {
+            // Deposit REI to Airdrop contract
+            const depositAmount = 1
+            await signer.sendTransaction({ to: airdrop.address, value: toWei(depositAmount) })
+
+            // Mock claimer data
+            const id = 0
+            const claimAmount = 0.1
+            const deadline = Date.now() + 60 // + 1 minute 
+
+            // hash data 
+            const hash = ethers.utils.solidityKeccak256(
+                ["address", "uint", "uint", "uint"],
+                [await claimer.getAddress(), id, toWei(claimAmount), deadline]
+            )
+            const hashBinary = ethers.utils.arrayify(hash)
+
+            // signer sign message
+            const signature = await signer.signMessage(hashBinary)
+
+            const tx = await airdrop.connect(claimer).claim(id, toWei(claimAmount), deadline, signature)
+            const txComfirmed = await tx.wait()
+            const event = txComfirmed.events?.find(e => e.event === 'Claim')
+            const eventArg = event?.args
+
+            expect(eventArg?.user).to.eq(await claimer.getAddress(), "Claimer address is wrong.")
+            expect(eventArg?.id).to.eq(id, "Claimer id is wrong.")
+            expect(fromWei(eventArg?.amount)).to.eq(claimAmount, "Claimer amount is wrong.")
+        })
+    })
+})
